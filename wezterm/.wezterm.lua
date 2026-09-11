d5c269685b5f929dea8e54dfc9e3fdac425c00f4
Paul Fischer <pfischer8989@gmail.com>
2026-09-11 12:20:23 -0700

    add tilebar back

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,7 +8,7 @@ config.font_size = 10.0
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "NONE"
+config.window_decorations = "TITLE | RESIZE"
 
 config.window_frame = {
   inactive_titlebar_bg = '#353535',
